d4e5ade180d923c1f16d5afb3005c1921e9c1fc7
Elantris <j88107333@gmail.com>
2019-01-14 12:43:26 +0800

    No response without correct parameters

--- command/cmdRes.js
+++ command/cmdRes.js
@@ -5,12 +5,15 @@ module.exports = ({ res, message, args }) => {
 
   let resList = Object.keys(res[message.guild.id][args[1]])
 
-  if (resList) {
-    let choice = resList[Math.floor(Math.random() * resList.length)] // random pick a response from list
-    if (args.length >= 3 && Number.isSafeInteger(parseInt(args[2])) && res[message.guild.id][args[1]][parseInt(args[2])]) {
-      choice = parseInt(args[2]) // pick specific response
+  let choice = resList[Math.floor(Math.random() * resList.length)] // random pick a response from list
+
+  if (args.length >= 3) {
+    if (!Number.isSafeInteger(parseInt(args[2])) || !res[message.guild.id][args[1]][parseInt(args[2])]) {
+      return
     }
 
-    message.channel.send(res[message.guild.id][args[1]][choice])
+    choice = parseInt(args[2]) // pick specific response
   }
+
+  message.channel.send(res[message.guild.id][args[1]][choice])
 }
