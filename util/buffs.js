9ed49e2f8e33b365a42981109089e4ff8ebcd53d
Elantris <j88107333@gmail.com>
2019-04-09 15:08:56 +0800

    Fix item id of buffs

--- util/buffs.js
+++ util/buffs.js
@@ -4,13 +4,13 @@ module.exports = {
     icon: ':bug:',
     displayName: '魚餌',
     duration: 3600000,
-    itemId: 18
+    itemId: '18'
   },
   '%1': {
     name: 'candy',
     icon: ':candy:',
     displayName: '好棒棒軟糖',
     duration: 3600000,
-    itemId: 19
+    itemId: '19'
   }
 }
