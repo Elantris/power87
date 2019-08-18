98a4881e0ae2424a0544c1bfce2879761ec212ea
Elantris <j88107333@gmail.com>
2019-08-18 15:48:14 +0800

    Update rules of command slot

--- command/slot.js
+++ command/slot.js
@@ -1,10 +1,18 @@
 const moment = require('moment')
 const energySystem = require('../util/energySystem')
 const inventorySystem = require('../util/inventorySystem')
+const items = require('../util/items')
 
+const buffMapping = {
+  '%1': ':candy:',
+  '%2': ':lollipop:',
+  '%3': ':chocolate_bar:',
+  '%4': ':popcorn:'
+}
+const baseHitChance = 0.08
 const symbols = [':gem:', ':seven:', ':trophy:', ':moneybag:', ':gift:', ':ribbon:', ':balloon:', ':four_leaf_clover:', ':battery:', ':dollar:']
 const prizes = [
-  { chance: 0.2577, pattern: '88', multiplier: 1 },
+  { chance: 0.2576, pattern: '88', multiplier: 1 },
   { chance: 0.2000, pattern: '77', multiplier: 2 },
   { chance: 0.1600, pattern: '66', multiplier: 5 },
   { chance: 0.1200, pattern: '55', multiplier: 7 },
@@ -13,7 +21,6 @@ const prizes = [
   { chance: 0.0200, pattern: '22', multiplier: 25 },
   { chance: 0.0100, pattern: '11', multiplier: 38 },
   { chance: 0.0100, pattern: '00', multiplier: 50 },
-
   { chance: 0.0512, pattern: '999', multiplier: 1 },
   { chance: 0.0256, pattern: '888', multiplier: 3 },
   { chance: 0.0128, pattern: '777', multiplier: 5 },
@@ -25,7 +32,13 @@ const prizes = [
   { chance: 0.0002, pattern: '111', multiplier: 77 },
   { chance: 0.0001, pattern: '000', multiplier: 100 }
 ]
-const baseHitChance = 0.08
+const template = `:slot_machine: {{USERNAME}} {{SAY}}投注了 {{ENERGY}} 點八七能量
+{{BUFF}}{{MARK}}
+-------------------
+{{SLOTS}}
+-------------------
+{{RESULT}}
+`
 const lostMessages = [
   '結果是一無所獲',
   '然而什麼都沒有',
@@ -35,14 +48,10 @@ const lostMessages = [
   '這些能量都進了許願池',
   '難過的是放棄的夢被打碎',
   '呃啊啊啊啊啊',
-  '獲得更多的印章'
+  '幸運之神憐憫地看著你',
+  '八七點數回收計劃非常成功',
+  '這是一句好話再試一下'
 ]
-const buffMapping = {
-  '%1': ':candy:',
-  '%2': ':lollipop:',
-  '%3': ':chocolate_bar:',
-  '%4': ':popcorn:'
-}
 
 module.exports = async ({ args, database, message, guildId, userId }) => {
   let energyCost = 1
@@ -64,7 +73,6 @@ module.exports = async ({ args, database, message, guildId, userId }) => {
       if (sayMessage.length > 50) {
         return { errorCode: 'ERROR_LENGTH_EXCEED' }
       }
-      sayMessage = `說完「${sayMessage}」之後`
     }
   }
 
@@ -76,38 +84,37 @@ module.exports = async ({ args, database, message, guildId, userId }) => {
     userEnergy = energySystem.INITIAL_USER_ENERGY
     database.ref(`/energy/${guildId}/${userId}`).set(userEnergy)
   }
+
   if (userEnergy < energyCost) {
     return { errorCode: 'ERROR_NO_ENERGY' }
   }
 
   // inventory system
   const userInventory = await inventorySystem.read(database, guildId, userId, message.createdTimestamp)
+  userInventory.items['47'] = userInventory.items['47'] || 0
 
-  let toolChance = 0
-  let buffChance = 0
-  let markChance = 0
+  const luckySevelLevel = parseInt(userInventory.tools['$4'] || -1)
+  const markNum = userInventory.items['47']
+
+  let chance = baseHitChance + (luckySevelLevel + 1) * 0.01 + markNum * 0.001
   let buffInUse = ''
-
-  if (userInventory.tools['$4']) {
-    toolChance = parseInt(userInventory.tools['$4'] + 1) * 0.01
-  }
+  let buffLastTime
 
   if (userInventory.buffs['%4']) {
+    chance += 0.04
     buffInUse = '%4'
-    buffChance = 0.08
   } else if (userInventory.buffs['%3']) {
+    chance += 0.03
     buffInUse = '%3'
-    buffChance = 0.04
   } else if (userInventory.buffs['%2']) {
+    chance += 0.02
     buffInUse = '%2'
-    buffChance = 0.02
   } else if (userInventory.buffs['%1']) {
+    chance += 0.01
     buffInUse = '%1'
-    buffChance = 0.01
   }
-
-  if (userInventory.items['47']) {
-    markChance = userInventory.items['47'] * 0.01
+  if (buffInUse) {
+    buffLastTime = moment.duration(userInventory.buffs[buffInUse] - message.createdTimestamp)
   }
 
   // slot
@@ -116,16 +123,17 @@ module.exports = async ({ args, database, message, guildId, userId }) => {
   let energyGain = 0
 
   let luck = Math.random()
-  if (luck < baseHitChance + toolChance + buffChance + markChance) {
+  if (luck < chance) {
     luck = Math.random()
-    prizes.some((prize, index) => {
-      if (luck < prize.chance) {
+
+    for (const index in prizes) {
+      if (luck < prizes[index].chance) {
         winId = index
-        return true
+        break
       }
-      luck -= prize.chance
-      return false
-    })
+      luck -= prizes[index].chance
+    }
+
     if (winId === -1) {
       winId = prizes.length - 1
     }
@@ -142,12 +150,13 @@ module.exports = async ({ args, database, message, guildId, userId }) => {
   }
 
   if (winId === -1) {
-    if (!userInventory.items['47']) {
-      userInventory.items['47'] = 0
-    }
     if (energyCost === 500) {
-      userInventory.items['47'] += 2
+      userInventory.items['47'] += 5
+    } else if (energyCost >= 250) {
+      userInventory.items['47'] += 4
     } else if (energyCost >= 50) {
+      userInventory.items['47'] += 2
+    } else if (energyCost >= 10) {
       userInventory.items['47'] += 1
     }
   } else {
@@ -160,38 +169,31 @@ module.exports = async ({ args, database, message, guildId, userId }) => {
 
   // response
   let content
-  let description = `:slot_machine: 這是一台八七拉霸機\n\n` +
-    `${message.member.displayName} ${sayMessage} 投注了 ${energyCost} 點八七能量\n` +
-    `增益效果：`
-
-  if (buffInUse) {
-    const buffTime = moment.duration(userInventory.buffs[buffInUse] - message.createdTimestamp)
-    description += `${buffMapping[buffInUse]} ${Math.floor(buffTime.asHours()).toString().padStart(2, '0')}:${buffTime.minutes().toString().padStart(2, '0')}`
-  }
-
-  description += `\n-------------------\n` +
-    slotResults.map(v => symbols[v]).join(' : ') +
-    `\n-------------------\n`
-
-  if (winId === -1) {
-    description += `| : : : : **LOST** : : : : |\n\n` +
-      lostMessages[Math.floor(Math.random() * lostMessages.length)]
-
-    if (userInventory.items['47']) {
-      description += `\n\n目前累積 :broken_heart:**失落的印章-迷惘賭徒**x${userInventory.items['47']}`
-    }
-  } else if (winId === 18) {
-    content = '@here 頭獎快訊！'
-    description += `| : **CONGRATS** : |\n\n` +
-      `<@${message.author.id}> 或成最大贏家，獲得了**頭獎** ${energyGain} 點八七能量`
-  } else if (winId === 17) {
-    content = '@here 777！'
-    description += `| : : **77777777** : : |\n\n` +
-      `<@${message.author.id}> 7 起來，獲得了 **777獎** ${energyGain} 點八七能量`
-  } else {
-    description += `| : : : : **WIN** : : : : : |\n\n` +
-      `贏得了 ${energyGain} 點八七能量`
-  }
+  const description = template
+    .replace('{{USERNAME}}', message.member.displayName)
+    .replace('{{SAY}}', sayMessage.length ? `說完「${sayMessage}」之後` : '')
+    .replace('{{ENERGY}}', energyCost)
+    .replace('{{BUFF}}', buffInUse ? `\n增益效果：${buffMapping[buffInUse]} ${buffLastTime.asHours().toFixed(0).toString().padStart(2, '0')}:${buffLastTime.minutes().toString().padStart(2, '0')}` : '')
+    .replace('{{MARK}}', markNum ? `\n累積印章：${items[47].icon}x${markNum}` : '')
+    .replace('{{SLOTS}}', slotResults.map(v => symbols[v]).join(' : '))
+    .replace('{{RESULT}}', () => {
+      if (winId === -1) {
+        return `| : : : : **LOST** : : : : |\n\n` +
+          lostMessages[Math.floor(Math.random() * lostMessages.length)] +
+          (userInventory.items['47'] - markNum ? `，獲得 ${items[47].icon}**${items[47].displayName}**x${userInventory.items['47'] - markNum}` : '')
+      } else if (winId === 18) {
+        content = '@here 頭獎快訊！'
+        return `| : **CONGRATS** : |\n\n` +
+          `<@${message.author.id}> 或成最大贏家，獲得了**頭獎** ${energyGain} 點八七能量`
+      } else if (winId === 17) {
+        content = '@here 777！'
+        return `| : : **77777777** : : |\n\n` +
+          `<@${message.author.id}> 7 起來，獲得了 **777獎** ${energyGain} 點八七能量`
+      } else {
+        return `| : : : : **WIN** : : : : : |\n\n` +
+          `贏得了 ${energyGain} 點八七能量`
+      }
+    })
 
   return { content, description }
 }
--- util/tools.js
+++ util/tools.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'bag',
     icon: ':school_satchel:',
     displayName: '隨身背包',
-    description: '增加物品欄位以存放更多物品，每提升一級解鎖 8 個物品欄位、1 個英雄裝備欄位。',
+    description: '增加物品欄位以存放更多物品，每提升一級可以增加 8 個物品欄位、1 個英雄裝備欄位。',
     maxLevel: 10,
     prices: [100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200, 102400]
   },
@@ -11,7 +11,7 @@ module.exports = {
     name: 'fishingpole',
     icon: ':fishing_pole_and_fish:',
     displayName: '釣竿',
-    description: '購買後才能釣魚，提升等級可以增加整體釣魚機率與減少釣魚時間。',
+    description: '釣魚前必須購買的裝備，提升等級時增加所有魚種獲得的機率、增加一次釣起多隻魚的機率。',
     maxLevel: 7,
     prices: [100, 300, 900, 2700, 8100, 24300, 72900, 218700]
   },
@@ -27,7 +27,7 @@ module.exports = {
     name: 'buoy',
     icon: ':trident:',
     displayName: '浮標',
-    description: '提升稀有魚種獲得機率。',
+    description: '吸引高品質的魚類跑來上鉤，提升等級時增加稀有魚種獲得的機率。',
     maxLevel: 3,
     prices: [150, 450, 1350, 4050]
   },
@@ -35,7 +35,7 @@ module.exports = {
     name: 'lucky-seven',
     icon: ':seven:',
     displayName: '幸運七',
-    description: '古代文明遺留的聖物，可以永久提升拉霸中獎機率。',
+    description: '古代文明遺留的聖物，提升等級時永久增加拉霸機中獎機率。',
     maxLevel: 3,
     prices: [1000, 10000, 100000, 1000000]
   }
